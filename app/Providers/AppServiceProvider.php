5d0976ce8cfae16aac648c220d759d8b99f3c832
Mathias Grimm <mathiasgrimm@gmail.com>
2025-07-01 11:48:34 -0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,11 +11,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->booting(static function (): void {
-            if (config('filesystems.laravel_cloud_disk_config')) {
-                $_SERVER['LARAVEL_CLOUD_DISK_CONFIG'] = config('filesystems.laravel_cloud_disk_config');
-            }
-        });
+        // $this->app->booting(static function (): void {
+        //     if (config('filesystems.laravel_cloud_disk_config')) {
+        //         $_SERVER['LARAVEL_CLOUD_DISK_CONFIG'] = config('filesystems.laravel_cloud_disk_config');
+        //     }
+        // });
     }
 
     /**
